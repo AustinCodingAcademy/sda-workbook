5206b7272968c238bcfcf3cec26f02f2af2a1c25
Roberto <roberto@austincodingacademy.com>
2016-10-13 02:28:50 +0000

    created public directory

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('serve', function(){
     var options = {
-        server: {baseDir: '_site/'},
+        server: {baseDir: 'public/'},
         port: process.env.PORT,
         ui: { port: 8081 }
     };
     browserSync.init(options);
-    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+    gulp.watch('public/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
