53d8b3557a15c8bdf00436c8f4dfaf1fed16dcdf
lisanadelatorre <ldelatorre00@gmail.com>
2016-12-20 19:09:19 +0000

    Lesson08 Labs Completed

---
+++ public/Lab-Files/Lesson08/Optional_Lab_8-1/hello.php
@@ -0,0 +1,33 @@
+<?php 
+$ua = $_SERVER['HTTP_USER_AGENT'];
+
+{print"
+
+
+<html>
+
+<head>
+
+<title>Browser detection with PHP</title>
+
+</head>
+
+<body>
+
+<h1>
+Hello, World!
+</h1>
+
+Your user agent is:
+<strong>
+{$ua}.
+</strong>
+
+</body>
+
+</html>
+
+";
+}
+
+?>
